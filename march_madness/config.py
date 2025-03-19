61874ff4ec20a89070a72b5f14d82aaf9ea0e1bb
kekoasantana <kekoasantana42@gmail.com>
2025-03-18 17:07:45 -0700

    modifying starting year

--- march_madness/config.py
+++ march_madness/config.py
@@ -1,7 +1,7 @@
 # Configuration settings
 RANDOM_STATE = 3
-STARTING_SEASON = 2010
+STARTING_SEASON = 2014
 CURRENT_SEASON = 2024
-TRAINING_SEASONS = list(range(2010, 2019))  # 2015-2018 for training
+TRAINING_SEASONS = list(range(2014, 2019))  # 2015-2018 for training
 VALIDATION_SEASON = 2019                   # 2019 for validation
 PREDICTION_SEASONS = [2021, 2022, 2023,2024]  # 2021-2024 for prediction
